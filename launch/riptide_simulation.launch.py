03b406e0b5ccb0352c89b2ba451a83bce45b46f6
Teusner <quentin.brateau@ensta-bretagne.org>
2023-08-31 12:08:11 +0200

    Updating to launch in gazebo

--- launch/riptide_simulation.launch.py
+++ launch/riptide_simulation.launch.py
@@ -9,6 +9,8 @@ from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 from launch_ros.actions import Node
 
@@ -20,7 +22,7 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     # Launch Arguments
-    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+    use_sim_time = LaunchConfiguration('use_sim_time', default=False)
 
     ld.add_action(
         DeclareLaunchArgument(
@@ -30,21 +32,35 @@ def generate_launch_description():
         )
     )
 
-
     # Riptide SDF file
     riptide_description_sim_package = os.path.join(get_package_share_directory('riptide_description_sim'))
-    model_file = os.path.join(riptide_description_sim_package, 'model.sdf')
+    xacro_file = os.path.join(riptide_description_sim_package, 'urdf', 'riptide.urdf.xacro')
+
+    doc = xacro.process_file(xacro_file, mappings={"prefix": "riptide_1"})
+    xacro.process_doc(doc)
+    params = {'robot_description': doc.toxml(), 'use_sim_time': use_sim_time}
+
+    # Robot State Publisher
+    ld.add_action(Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': doc.toxml(), 'ignore_timestamp': True}]
+        )
+    )
 
     # Riptide spawner
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        namespace="riptide_1",
+        arguments=['-string', doc.toxml(),
+                '-name', 'riptide',
+                '-allow_renaming', 'true'],
+    )
     ld.add_action(
-        Node(
-            package='ros_gz_sim',
-            executable='create',
-            output='screen',
-            arguments=['-file', model_file,
-                    '-name', 'riptide',
-                    '-allow_renaming', 'true'],
-        )
+        spawn_entity
     )
 
     # World
@@ -54,9 +70,26 @@ def generate_launch_description():
     ld.add_action(
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                [os.path.join(get_package_share_directory('ros_gz_sim'),
-                              'launch', 'gz_sim.launch.py')]),
+                [os.path.join(get_package_share_directory('ros_ign_gazebo'),
+                              'launch', 'ign_gazebo.launch.py')]),
             launch_arguments=[('gz_args', [' -r -v 4 ' + world_file])]
+        )
+    )
+
+    # Joint_state_broadcaster
+    load_joint_state_broadcaster = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster'],
+        output='screen',
+    )
+
+    ld.add_action(
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_broadcaster],
+            )
         )
     )
     
